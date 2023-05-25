780d0c1a1e8824a034e2b43637e1c08d06c3129d
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-05-25 17:40:07 +0530

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,8 +11,7 @@ export async function before(m, { conn }) {
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`
-*▢ SUBISTE DE NIVEL*
-
+*▢ LEVEL UP*
  *${before}* ‣  *${user.level}*
  Rango : *${user.role}*
  
