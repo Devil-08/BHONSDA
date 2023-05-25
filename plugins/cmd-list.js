af95355a00e436a797678854c23215d36e9060cd
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-05-25 17:22:03 +0530

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
 *LISTA DE  COMANDOS*
 
-▢ *Info:* Si esta en *negrita*  esta bloqueado
+▢ *Info:*If it is in *bold* it is blocked
 
 ──────────────────
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n')}
